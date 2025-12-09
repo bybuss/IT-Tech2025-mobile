b143e69b3183a666ad65988b53854ccc48ff9878
bybuss <kukuhaha729@gmail.com>
2025-12-09 18:53:22 +0300

    deleted: old auth with email & password

--- app/src/main/java/bob/colbaskin/it_tech2025/auth/data/models/RegisterBody.kt
+++
@@ -1,9 +0,0 @@
-package bob.colbaskin.it_tech2025.auth.data.models
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class RegisterBody(
-    val email: String,
-    val password: String
-)
--- app/src/main/java/bob/colbaskin/it_tech2025/auth/presentation/sign_up/SignUpAction.kt
+++
@@ -1,8 +0,0 @@
-package bob.colbaskin.it_tech2025.auth.presentation.sign_up
-
-interface SignUpAction {
-    data object SignIn : SignUpAction
-    data object SignUp : SignUpAction
-    data class UpdateEmail(val email: String): SignUpAction
-    data class UpdatePassword(val password: String): SignUpAction
-}
