336af16a1ea8cb58ca5551eb2677921e538f6ce6
bybuss <kukuhaha729@gmail.com>
2025-12-10 04:37:20 +0300

    added: scanner

--- app/src/main/java/bob/colbaskin/it_tech2025/Application.kt
+++ app/src/main/java/bob/colbaskin/it_tech2025/Application.kt
@@ -1,7 +1,21 @@
 package bob.colbaskin.it_tech2025
 
 import android.app.Application
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
+import io.scanbot.sdk.barcode_scanner.ScanbotBarcodeScannerSDKInitializer
 
 @HiltAndroidApp
-class Application: Application()
+class Application: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        try {
+            ScanbotBarcodeScannerSDKInitializer()
+                .initialize(this)
+        } catch (e: Exception) {
+            Log.e("ScannerApplication", "Ошибка инициализации Scanbot SDK", e)
+        }
+    }
+}
