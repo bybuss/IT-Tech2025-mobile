b801e19f44b87bb649b6f9a35c3f569dc00816b2
bybuss <kukuhaha729@gmail.com>
2025-12-10 14:57:20 +0300

    added: logout request

--- app/src/main/java/bob/colbaskin/it_tech2025/profile/domain/ProfileRepository.kt
+++ app/src/main/java/bob/colbaskin/it_tech2025/profile/domain/ProfileRepository.kt
@@ -2,8 +2,9 @@ package bob.colbaskin.it_tech2025.profile.domain
 
 import bob.colbaskin.it_tech2025.common.ApiResult
 import bob.colbaskin.it_tech2025.profile.domain.models.User
+import retrofit2.Response
 
 interface ProfileRepository {
-
     suspend fun getUser(): ApiResult<User>
+    suspend fun logout(): ApiResult<Unit>
 }
--- app/src/main/java/bob/colbaskin/it_tech2025/profile/domain/ProfileService.kt
+++ app/src/main/java/bob/colbaskin/it_tech2025/profile/domain/ProfileService.kt
@@ -1,10 +1,15 @@
 package bob.colbaskin.it_tech2025.profile.domain
 
 import bob.colbaskin.it_tech2025.profile.data.models.UserDTO
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ProfileService {
 
     @GET("/api/v1/user/me")
     suspend fun getUser(): UserDTO
+
+    @POST("/api/v1/auth/logout")
+    suspend fun logout(): Response<Unit>
 }
